87ba218d3e8911c222b43ff0430082cdfa351859
shivank55 <shivankjain555@gmail.com>
2024-06-20 15:44:36 +0530

    changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Navbar.css' 
-
+import logo from "../assets/logo.png"
+import cart from '../assets/cart_icon.png'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'> 
+       <div className="nav-logo"></div>
+    </div>
   )
 }
