00808f9a151e05de067fedceca31f4bc500cf20a
shivank55 <shivankjain555@gmail.com>
2024-06-24 09:48:58 +0530

    worked on item.css

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -7,10 +7,10 @@ const Item = (props) => {
       <img src={props.image} alt='' />
       <p>{props.name}</p>
       <div className="item-prices">
-        <div className="item-prices-new">
+        <div className="item-price-new">
            {props.new_price}
         </div>
-        <div className="item-prices-old">
+        <div className="item-price-old">
             {props.old_price}
         </div>
       </div>
