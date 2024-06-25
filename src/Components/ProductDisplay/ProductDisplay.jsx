c931b2613703f3e3bfb93beff28013b25aa7781a
shivank55 <shivankjain555@gmail.com>
2024-06-25 23:52:13 +0530

    worked on files

---
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './ProductDisplay.css'
+import star_icon from '../assets/star_icon.png'
+import star_dull_icon from '../assets/star_dull_icon.png'
+
+const ProductDisplay = (props) => {
+    const {product} = props
+  return (
+    <div className='productdisplay'>
+       <div className="productdisplay-left">
+        <div className="productdisplay-img-list">
+            <img src={product.image} alt="" />
+            <img src={product.image} alt="" />
+            <img src={product.image} alt="" />
+            <img src={product.image} alt="" />
+        </div>
+        <div className="productdisplay-img">
+            <img className='productdisplay-main-img' src={product.image} alt="" />
+        </div>
+       </div>
+       <div className="productdisplay-right">
+            <h1>{product.name}</h1>
+              <div className="productdisplay-right-star">
+                  <img src={star_icon} alt="" />
+                  <img src={star_icon} alt="" />
+                  <img src={star_icon} alt="" />
+                  <img src={star_icon} alt="" />
+                  <img src={star_dull_icon} alt="" />
+                  <p>(7755)</p>
+              </div>
+              <div className="productdisplay-right-prices">
+                <div className="productdisplay-right-prices-old">${product.old_price}</div>
+                <div className="productdisplay-right-prices-new">${product.new_price}</div>
+              </div>
+              <div className="productdisplay-right-description">
+                  A lightweight, usually knitted, pullover shirt, close-fitting, and with
+                  a rounded neckline and short sleeves, worn as an undershirt or outer 
+                  garment. 
+              </div>
+              <div className="productdisplay-right-size">
+                <h1></h1>
+              </div>
+       </div>
+    </div>
+  )
+}
+
+export default ProductDisplay
