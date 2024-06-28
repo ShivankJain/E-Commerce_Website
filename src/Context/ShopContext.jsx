f8b3ef26913b17aaf7cd1f10b3a116f7ca1b633b
shivank55 <shivankjain555@gmail.com>
2024-06-28 17:33:36 +0530

    project completed

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -28,11 +28,23 @@ const ShopContextProvider = (props) => {
         for(const item in cartItems){ 
            if(cartItems[item]>0){ 
             let itemInfo = all_product.find((product)=>product.id===Number(item))
+            totalAmount += itemInfo.new_price * cartItems[item]
            } 
         }
+        return totalAmount
      }
 
-     const contextValue = {all_product,cartItems,addToCart,removeFromCart}
+     const getTotalCartItems = () =>{ 
+        let totalItems = 0
+        for(const item in cartItems){ 
+            if(cartItems[item]>0){ 
+                totalItems += cartItems[item];
+            }
+        }
+        return totalItems;
+     }
+
+     const contextValue = {getTotalCartItems,getTotalCartAmount,all_product,cartItems,addToCart,removeFromCart}
 
       return(
         <ShopContext.Provider value={contextValue}>
